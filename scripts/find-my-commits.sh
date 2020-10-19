6db9cfcf21e039426eaf84203671312412dac1d3
Thibaud Ledent <thibaudledent@gmail.com>
2020-10-19 10:32:04 +0200

    Java 14 and key binding for CTRL+U

--- scripts/find-my-commits.sh
+++ scripts/find-my-commits.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-set -eEuxo pipefail
 
 GIT_DIRECTORY=/path/to/my/folder/with/git/repositories
 NAME=myname
-START_DATE=2020-04-05
+START_DATE=2020-10-02
 
 cd $GIT_DIRECTORY || exit
 rm -f /tmp/gitlog
@@ -12,7 +11,7 @@ find . -type d -name .git | while read -r i;
 do 
     DIR=$(dirname "$i")
     cd "$DIR" || exit
-    git log --pretty=format:"%at %ai %ae %s" --since=="${START_DATE}T00:00:00-06:00" | grep -i $NAME | grep -Ev "Merge|Revert" >> /tmp/gitlog
+    git log --pretty=format:"%at %ai %ae %s" --since="${START_DATE}T00:00:00" | grep -i $NAME | grep -Ev "Merge|Revert" >> /tmp/gitlog
     cd $GIT_DIRECTORY || exit
 done
 
