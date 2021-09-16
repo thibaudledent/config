f7bf567e160766c4a15954d7a06d3e349b6a2114
Thibaud Ledent <thibaudledent@gmail.com>
2021-09-16 14:25:11 +0200

    Default size for password generation: 128

--- scripts/generate-password.sh
+++ scripts/generate-password.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eEuo pipefail
 
-docker run --rm -it debian bash -c "</dev/urandom tr -dc 'A-Za-z0-9!#$%&'\''()*+,-./:;<=>?@[\]^_{}~' | head -c ${1:-32} ; echo"
+docker run --rm -it debian bash -c "</dev/urandom tr -dc 'A-Za-z0-9!#$%&'\''()*+,-./:;<=>?@[\]^_{}~' | head -c ${1:-128} ; echo"
