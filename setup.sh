c92e196746855238d8b345d5b89dc3f940a41720
Thibaud Ledent <thibaudledent@gmail.com>
2024-11-20 13:26:53 +0100

    Update config for WSL

--- setup.sh
+++ setup.sh
@@ -6,10 +6,12 @@ set -Eeuxo pipefail
 LN=$(which ln)
 LN_OPTS="-f -s -v"
 
+# Tools
+sudo apt update && sudo apt install tldr unzip zip zsh fzf terminator curl -y
+
 # ZSH
 if [ ! -d "$HOME/.oh-my-zsh" ];
 then
-  sudo apt update && sudo apt install zsh fzf terminator curl -y
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
   git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
@@ -21,6 +23,27 @@ then
   # make zsh your default shell
   chsh -s "$(which zsh)"
   # then log out and log back in
+fi
+
+# SDK man (JAVA sdk and maven)
+if [ ! -f "$SDKMAN_DIR/bin/sdkman-init.sh" ];
+then
+  if [ -f "/etc/wsl.conf" ];
+  then
+    # todo fix install in WSL, error in intellij cannot find C:\programs\sdkman\candidates\java\...\bin\java.exe
+    #export SDKMAN_DIR="/mnt/c/programs/sdkman" && curl -s "https://get.sdkman.io" | bash
+    #source "$SDKMAN_DIR/bin/sdkman-init.sh"
+  else
+    curl -s "https://get.sdkman.io" | bash
+    source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+    sdk install java 23-tem
+
+    # todo move these lines out of else block when WSL install is fixed
+    echo "y" |sdk install java 17.0.12-oracle # y for yes to set as default jdk
+    echo "n" | sdk install java
+    sdk install maven
+  fi
 fi
 
 # shellcheck disable=SC2086 # ignore "Double quote to prevent globbing and word splitting" for $LN_OPTS
@@ -47,7 +70,14 @@ fi
 
 # MAVEN SETTINGS
 # Overriding your Maven user settings in ${user.home}/.m2/settings.xml
-if [ -e ~/.m2/settings.xml ]
+if [ -f "/etc/wsl.conf" ];
+then
+  CUSTOM_HOME=$(wslpath "$(cmd.exe /C "echo %USERPROFILE%" 2>/dev/null | tr -d '\r')")
+else
+  CUSTOM_HOME=$HOME
+fi
+
+if [ ! -f "$CUSTOM_HOME/.m2/settings.xml" ];
 then
   set +x
   echo -n "Enter your Maven settings.xml artifactory user: "
@@ -61,9 +91,12 @@ then
   sed -e "s/USER_LOGIN/$user/" -e "s/USER_PASSWORD/$key/"  -e "s#ARTIFACTORY_URL#$url#" "${PWD}"/maven/settings.xml > "${PWD}"/maven/settings_with_api_key.xml
   set -x
 
-  # shellcheck disable=SC2086 # ignore "Double quote to prevent globbing and word splitting" for $LN_OPTS
   mkdir -p ~/.m2
+  # shellcheck disable=SC2086 # ignore "Double quote to prevent globbing and word splitting" for $LN_OPTS
   "$LN" $LN_OPTS "${PWD}"/maven/settings_with_api_key.xml ~/.m2/settings.xml
+  if [ -f "/etc/wsl.conf" ]; then
+    cp "${PWD}/maven/settings_with_api_key.xml" "$CUSTOM_HOME/.m2/settings.xml"
+  fi
 fi
 
 echo "Setup done!"
